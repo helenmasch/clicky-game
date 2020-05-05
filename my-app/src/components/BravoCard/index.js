429dd47cae659c3ccb4d361ffe5bf8da8896c2ca
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-05-05 16:06:18 -0400

    added component folders

--- my-app/src/components/BravoCard/index.js
+++ my-app/src/components/BravoCard/index.js
@@ -2,6 +2,4 @@ import React, { Component } from "react";
 import FriendCard from "./components/BravoCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/header";
-
-
-//import friends from "./friends.json";
+import friends from "./bravoimages.json";
