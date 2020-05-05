730f3c81fc25265e2c9a974d8c93740abe813cf9
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-05-05 15:40:43 -0400

    started to add images into json file

---
+++ my-app/src/components/BravoCard/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import FriendCard from "./components/BravoCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/header";
+
+
+//import friends from "./friends.json";
