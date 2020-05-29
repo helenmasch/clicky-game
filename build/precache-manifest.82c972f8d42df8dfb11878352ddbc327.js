43e9a24bdb9409b4b342c88ef038dbfbb0d2259b
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-05-29 13:39:15 -0400

    new branch

---
+++ build/precache-manifest.82c972f8d42df8dfb11878352ddbc327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bba0cd891d16feab8f0a0abbcd936b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ba21bff3a552de7a51cb",
+    "url": "/clicky-game/static/css/main.fb090c90.chunk.css"
+  },
+  {
+    "revision": "4dc571543c8c23f585aa",
+    "url": "/clicky-game/static/js/2.8001b0e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.8001b0e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba21bff3a552de7a51cb",
+    "url": "/clicky-game/static/js/main.4dd5bcff.chunk.js"
+  },
+  {
+    "revision": "cb2c9d13f26f7f1cb7bd",
+    "url": "/clicky-game/static/js/runtime-main.c0536369.js"
+  }
+]);
